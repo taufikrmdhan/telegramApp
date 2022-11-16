5d4bd74c41be7df1ab056e8e70384dd5baa9f2c3
Taufik Ramadhan <taufik.30@students.amikom.ac.id>
2022-11-16 14:54:43 +0700

    Private route

--- Frontend/src/router/index.jsx
+++ Frontend/src/router/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router-dom";
 
 import Register from "../pages/Register";
 import Login from "../pages/Login";
@@ -12,18 +12,47 @@ import Contact from "../pages/Contactinfo";
 import Update from "../pages/updateProfile";
 // import Chat from "../pages/Chat";
 
+const PrivateRoute = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    return <Outlet />;
+  } else {
+    alert("Please login first");
+    return <Navigate to="/" />;
+  }
+}
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route index element={<Login />} />
+        <Route path="register" element={<Register />} />
+        
+        <Route path="message" element={<PrivateRoute />}>
+          <Route index element={<Message />} />
+        </Route>
+        <Route path="profile" element={<PrivateRoute />}>
+          <Route index element={<Profile />} />
+        </Route>
+        <Route path="update" element={<PrivateRoute />}>
+          <Route index element={<Update />} />
+        </Route>
+        <Route path="chat" element={<PrivateRoute />}>
+          <Route index element={<Chat />} />
+        </Route>
+        <Route path="contact" element={<PrivateRoute />}>
+          <Route index element={<Contact />} />
+        </Route>
+        
+        {/* <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/splash" element={<Splash />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/message" element={<Message />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/update" element={<Update />} />
+        <Route path="/update" element={<Update />} /> */}
         {/* <Route path="/chat" element={<Chat />} /> */}
       </Routes>
     </BrowserRouter>
